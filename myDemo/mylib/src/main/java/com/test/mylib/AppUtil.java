30184411c2967ff7590348080b2b1050feb2aa07
ezyong <zhaoyong4435@navinfo.com>
2016-01-18 14:48:44 +0800

    add commit

--- myDemo/mylib/src/main/java/com/test/mylib/AppUtil.java
+++ myDemo/mylib/src/main/java/com/test/mylib/AppUtil.java
@@ -2,7 +2,11 @@ package com.test.mylib;
 
 
 public class AppUtil {
-
+    /**
+     * 
+     * @param txt
+     * @return
+     */
     public static boolean isEmpty(String txt){
         if (txt == null || txt.length() == 0){
             return true;
