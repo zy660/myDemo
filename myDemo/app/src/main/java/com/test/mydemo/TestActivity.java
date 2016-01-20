dee2394ea3d5147dca3dae214b88b6e747337919
zy660 <zy660@163.com>
2016-01-20 16:00:04 +0800

    add layout

--- myDemo/app/src/main/java/com/test/mydemo/TestActivity.java
+++ myDemo/app/src/main/java/com/test/mydemo/TestActivity.java
@@ -1,13 +1,48 @@
 package com.test.mydemo;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class TestActivity extends Activity {
+import com.test.mydemo.manage.ILogin;
+import com.test.mydemo.manage.LoginManageImpl;
+import com.test.mydemo.view.ILoginView;
 
+public class TestActivity extends Activity implements ILoginView{
+    private Button btnClick;
+    private EditText etUserName;
+    private EditText etPassword;
+    private ILogin iLoginManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        iLoginManager = new LoginManageImpl();
+        setContentView(R.layout.activity_main);
+        etUserName = (EditText)findViewById(R.id.et_username);
+        etPassword = (EditText)findViewById(R.id.et_password);
+        btnClick = (Button) findViewById(R.id.btn_click);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 
+    @Override
+    public String getUserName() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public void login() {
+
+    }
 }
--- myDemo/app/src/main/java/com/test/mydemo/manage/LoginManageImpl.java
+++ myDemo/app/src/main/java/com/test/mydemo/manage/LoginManageImpl.java
@@ -1,7 +1,13 @@
 package com.test.mydemo.manage;
 
 
+import com.test.mydemo.view.ILoginView;
+
 public class LoginManageImpl implements ILogin{
+    private ILoginView iLoginView;
+    public LoginManageImpl(ILoginView iLoginView){
+        this.iLoginView = iLoginView;
+    }
 
     @Override
     public void onLogin(String username, String password, OnLoginListener loginListener) {
